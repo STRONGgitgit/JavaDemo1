504fcd3083ddf0e81879c8a4c2347ae938387f5c
STRONGgitgit <42628589+STRONGgitgit@users.noreply.github.com>
2018-08-24 09:39:31 +0800

    Update JavaDemo1.java

--- src/JavaDemo1.java
+++ src/JavaDemo1.java
@@ -1,6 +1,8 @@
 public class JavaDemo1 {
     public static void main(String[] args) {
         System.out.println("Hello would");
+        JavaDemo1 java =new JavaDemo1();
+        java.add();
     }
     public void add(){
         int i = 0;
