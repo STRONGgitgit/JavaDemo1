16b212c7d5f5407e900616cec7b5c7b6fb9fe95c
STRONGgitgit <1024474540@qq.com>
2018-08-24 09:38:19 +0800

    添加了一个方法

--- src/JavaDemo1.java
+++ src/JavaDemo1.java
@@ -2,4 +2,9 @@ public class JavaDemo1 {
     public static void main(String[] args) {
         System.out.println("Hello would");
     }
+    public void add(){
+        int i = 0;
+        System.out.println(i);
+    }
+
 }
